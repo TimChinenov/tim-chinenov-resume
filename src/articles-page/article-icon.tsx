85ca833c3f10080a98aec6a021eede4853e1d2d2
TimChinenov <tim.chinenov@gmail.com>
2022-11-13 21:10:06 -0800

    Added story about UX

--- src/articles-page/article-icon.tsx
+++ src/articles-page/article-icon.tsx
@@ -5,9 +5,9 @@ export default function ArticleIcon({article = {} as Article}) {
     return (
         <div className="w-full h-full relative flex justify-center items-center">
             <p className="left-2 text-white absolute top-0 z-10">Issue No. {article.episode}</p>
-            <h2 className="text-center text-white z-10 p-2">{article.title}</h2>
+            <h2 className="text-center text-white z-10 p-10">{article.title}</h2>
             <img src={imagePath} className="absolute w-full h-full object-cover"></img>
-            <div className="absolute w-full h-full bg-black opacity-40 hover:opacity-20 transition ease-in-out"></div>
+            <div className="absolute w-full h-full bg-black opacity-60 hover:opacity-20 transition ease-in-out"></div>
         </div>
     )
 }
--- src/articles-service/registry.tsx
+++ src/articles-service/registry.tsx
@@ -3,7 +3,7 @@ import { Article } from "./articles-service";
 export const articles: Map<string, Article> = new Map([
     ["1", {
         referenceId: "1",
-        coverPhoto: "rivianvcyber.png",
+        coverPhoto: "rivian_v_cybertruck/rivianvcyber.png",
         title: "Rivian, Cybertruck, and The Cultural Implications of EV Design",
         subtitle: "A conversation with a former Tesla designer on the two vehicles",
         episode: 1,
@@ -14,27 +14,88 @@ export const articles: Map<string, Article> = new Map([
             "The UI/UX expert drew an analogy for me: the current battle between the two vehicles is reminiscent of the early days between Microsoft and Apple. The two companies were contending for similar markets, yet they had drastically different approaches to design.",
             "The design of the two companies reflected the world they wanted to create. Apple products always seemed to put design and user friendliness first. They wanted to shape a world that was sleek, clean, and a little fun. Their products were always intuitive and simple. They arguably invested more into design and creative software. Meanwhile, Microsoft saw a much more functional world — a world of corporate professionalism. Impressively smooth and powerful tools that also required equally powerful glasses to use.",
             {
-                fileName: "mac.jpeg",
+                fileName: "rivian_v_cybertruck/mac.jpeg",
                 subtext: "Sleek, clean, and colorful."
             },
             "Both companies were ultimately successful, but it is the design of Apple products that the world chose. It is the design of Apple products that were impressed onto the world. In the corporate environment we now wear eccentric colors and chic outfits instead of oversized brown and khaki suits. Our skyscrapers adopt elegant glass based geometries, while concrete brutalist architecture dies out. We drive sleek aerodynamic cars instead of the blocky cars of the past. Many other technologies, even those of Microsoft, have adopted the design elements of Apple.",
+            {
+                fileName: "rivian_v_cybertruck/suits.bmp",
+                subtext: "Famous picture demonstrates how suit style, fit, and color among NBA players changed between 2003 and 2017. There are cultural implications of the technology we adopt."
+            },
             "Similarly, the former Tesla engineer viewed the two vehicles as separate visions for what the companies wanted the world to be. The Rivian R1T is an adventure vehicle and wants its drivers to go out exploring with it. The car has an array of tires and suspension modes that facilitate driving through harsh terrain. It does not stand out in the wilderness either. The vehicle is designed with natural elements, such as rounded edges, natural soft curves, and Earth tone color palettes.",
             "The inside of the R1T continues to use natural elements through the use of modern wood paneling. The interior also demonstrates the utilitarian and sporty characteristics of the vehicle. In addition to common truck peripherals, such as outlets and additional batteries, the vehicle has built in compartments for a Rivian flash light, portable speaker set, and a whole pullout section for a stove and sink. This truck is meant to be driven into the wilderness and opened with all its doors to the world.",
+            {
+                fileName: "rivian_v_cybertruck/rivianColors.bmp",
+                subtext: "The Earth tones of the R1T"
+            },
             "The design of the R1T envisions a world where technology has not strayed from nature. Where even in the cyber enhanced world, we still explore and we still have fun. Our electric trucks are not dainty vehicles that must be reserved to the anthropogenic realm. They can be drudged through the swampy fields of Florida or rough ridden through the deserts of New Mexico. The addition of a portable speaker also sings out to the communal nature of the vehicle. This is a car that we drive with friends, park on the beach and get rowdy with. This is a car that we can make memories with.",
             "The design of the Cybertruck could not scream for an ever more opposite world. The Cybetruck has no smooth edges; it is a polygonal chunk of metal that does not sport a paint coat in its base model. The sharp edges of the truck look like they can cut you just by driving past.",
             "Upon initial inspection, one could possibly mistake the flat bed for an industrial or military vehicle. It would be a gaudy spectacle in any environment outside of an American city. Surrounded by trees in a forest, the truck would look like the left behind engine of an alien spacecraft. In the dense streets of a European city, the oversized truck would feel like a dangerous nuisance to pedestrians and intentionally iconoclastic to the surrounding traditional architecture. Only in the car catered, urban decayed cities of the United States would such a truck seem at home.",
             "Functionally, the vehicle is advertised as tough, for tough people. The Cybertruck is meant to have bulletproof glass windows and a body that can take a hit from a sledge hammer. Similar to Tesla\'s other vehicles, the Cybetruck will sport a “bio defense mode” that will engage an air filter to reduce toxic external particulates. Nothing says tough like a paranoia-inducing armored vehicle in your tranquil suburban neighborhood.",
+            {
+                fileName: "rivian_v_cybertruck/cybertruck.bmp",
+                subtext: "I suppose it has a Frank Lloyd Wright aesthetic."
+            },
             "Cybertruck is a post-apocalyptic survival tank, and the design projects a grim future. It\'s a future where we need a tank to drive places, because the world we\'ve made is so inhospitable. Whether it is crime or poor air quality, it envisions a world where one must ask themselves every morning whether it is safe to go outside. The vehicle's design comes from the cyberpunk genre that inspired grungy futuristic movies like Judge Dredd, Total Recall, and Punisher. These movies are set in dystopian futures where man has allowed technology to get the better of themselves.",
             "After this description, the former Tesla designer expressed, “I don\'t want to live in the world envisioned by the Cybertruck”. As much as he loved Tesla and their broader mission, he was rooting for the Rivian R1T.",
             "T.Y.C"
         ]
     }],
-    ["2", {
+    [
+        "2", {
         referenceId: "2",
-        coverPhoto: "",
+        coverPhoto: "social_media_ux_encourages_lunacy/coverPhoto.bmp",
+        title: "Social Media UX Encourages Lunacy",
+        subtitle: "How non-distinguishable user design makes reputability difficult to discern",
+        episode: 2,
+        body: [
+            "~This article contains modified and edited tweets. None of them reflect the actual online activity of the impersonated user.~",
+            "",
+            "Growing up in New York City, I'd occasionally run into the unfortunate case of finding myself in Time Square. While this area is the armpit of New York City tourism, it was also the home of what we called “the crazies”. The crazies were not hard to spot, they made themselves quite obvious. I have some examples below.",
+            {
+                fileName: "social_media_ux_encourages_lunacy/crazy_examples.bmp",
+                subtext: "Pretty sure two of these pictures are actually taken in New York City."
+            },
+            "You learn quickly in NYC that you don't want to get involved with anything that reaches the decibel level of oncoming traffic, and boy do these poster wielding warriors know how to yell. However, the general reasonable New Yorker can look at these people and immediately tell they are not reputable, factual, or worth the mental energy. Why is this?",
+            "Well it's all about presentation. The posters are often poorly designed or rushed, indicating the individual doesn't have much self-awareness (otherwise they probably wouldn't be partaking in their actions). Their aggressive shouting is not very inviting, causing people to frequently cross the streets to avoid interaction. People holding these signs are often alone or in small numbers. The actions of other bystanders that ignore these advertisers indicate to us that the larger population understands that these individuals are not worth the effort.",
+            "Yet, we fail this test everyday on the internet. Everyday we're faced with hundreds of crazies on Twitter, Reddit, Facebook, and other social media platforms. Well, lets take a look at the same cases.",
+            {
+                fileName: "social_media_ux_encourages_lunacy/elon_ufo.bmp",
+            },
+            {
+                fileName: "social_media_ux_encourages_lunacy/warren_covid.bmp",
+            },
+            {
+                fileName: "social_media_ux_encourages_lunacy/roberts.bmp",
+                subtext: "Sounds so festive, Chief Supreme Court Justice John Roberts!"
+            },
+            "It's not real, but don't tell me you couldn't see Elon tweeting something like that. The issue here is that some of those “bullshit sensing” queues we have in NYC are clogged with these tweets. First off, the presentation is the same for any tweet and any person. The font is the same. The background is the same. The color palette is the same. The only indicator I have is a little checkbox. But even then, I don't know the author of the tweet. Was it actually the individual or some poor intern carrying the entire weight of the marketing deparment on their back?",
+            "Regardless of the post, the tweet is often fairly inviting. All of our options are actually inviting! Look, we can “heart” the tweet. Or we can add our own little old opinion by clicking the tiny cute comment bubble. We can even “retweet” which looks a lot like the “recycle” icon. Who doesn't like a little bit of sustainability?",
+            "We don't see the actual reaction of bystanders interacting with the tweet. Sure we can see all the moronic responses and the number of likes. But, I don’t see how meany people are crossing the street to avoid this tweet. There's no “swiped past” counter (there's an idea).",
+            "So, we're left alone digesting asinine information. The app encompasses our attention so we don’t fact check what we view. Even worse, we may subconsciously refuse to fact check what we view in fear that we’re actually wrong. If you’re the trapped suburbanite, lonely soul, or just anxious of sharing your “hot takes”, you never get to see and feel the absolute dismissal of your ridiculous opinion from real people. This is the scornful look a New Yorker will give you if you block their path on the sidewalk. Or, just shout some dumb nonsense in their face.",
+            "Perhaps instead of simply differentiating between verified and unverified accounts, the posts of an account can actually represent the reputability of the account. What if we decreased the opacity of accounts that have a bad history? Maybe people will then gloss over the post.",
+            {
+                fileName: "social_media_ux_encourages_lunacy/dim_css_example.bmp",
+            },
+            "Or we can just change the entire color of the post to be less appealing to look at.",
+            {
+                fileName: "social_media_ux_encourages_lunacy/ugly_background_color.bmp",
+            },
+            "How about a fun modern twist with a use of emojis?",
+            {
+                fileName: "social_media_ux_encourages_lunacy/emoji_example.bmp",
+            },
+            "That is perfect. Now I definitely know which account is a bot.",
+            "We use visual queues everyday to determine the legitimacy of people's intentions. There’s a reason I don’t buy Gucci bags from street vendors. Why should we remove these signals on the internet?",
+            "T.Y.C"
+        ]
+    }],
+    ["3", {
+        referenceId: "3",
+        coverPhoto: "3",
         title: "Image Processing To Count Artillery Craters",
         subtitle: "A basic and simple approach to get a sense of damage",
-        episode: 2,
+        episode: 3,
         body: [
             "As someone with a diverse lineage of Easter European ancestry, the war in Ukraine hits close to home. I'm continuously monitoring news as it comes from the front lines and hearing from friends and relatives various updates. I was recently passed a set of images of some of the damage Russian artillery has done in Eastern Ukraine and was requested to use some of my image processing experience to estimate the number of craters in these images.",
             "Admittedly, I haven't done much image processing work since I left Tesla roughly two years ago. As a result, my approach was fairly experimental and rudimentary. Yet, I hope this approach demonstrates just how much information can be scrapped with a bit of image processing know-how. So let's dust off those old image processing lecture notes and get to it.",
@@ -47,12 +108,12 @@ export const articles: Map<string, Article> = new Map([
             "From the get-go, I could tell this photo has several elements that would make running any image processing algorithm across the whole image tricky. There are tree lines, roads, and some clouds that are interfering with parts of the image. To better isolate the clean regions of the image, I performed some basic spatial transformations.",
         ]
     }],
-    ["3", {
-        referenceId: "3",
+    ["4", {
+        referenceId: "4",
         coverPhoto: "",
         title: "The Lag — My Brief Experience Working in Financial Tech",
         subtitle: "",
-        episode: 3,
+        episode: 4,
         body: [
             "I believe it is important that I preface this article by stating that the opinions in this article are solely my own and do not represent that of any organization or other individuals.",
             "...",
